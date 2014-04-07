e4a9adb955c197d3e84a332ef3ffb53280e3254e
chenghao1994 <305959772@qq.com>
2014-04-07 23:01:53 +0800

    fist commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: CH
+ * Date: 14-4-7
+ * Time: 下午11:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
